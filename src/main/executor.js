13594121ce65c3448dd965a4f5f24ce8d89a1b34
Roland Bernard <rolli.bernard@gmail.com>
2020-07-01 15:42:24 +0200

    Added changes forgotten in the last commit

--- src/main/executor.js
+++ src/main/executor.js
@@ -1,5 +1,6 @@
 
 import MarvinQuoteModule from "./modules/marvin-quote";
+import { config } from "./config";
 
 const modules = {
     marvin_quote: MarvinQuoteModule,
@@ -8,7 +9,10 @@ const modules = {
 export async function searchQuery(query, callback) {
     let results = [];
     await Promise.all(Object.keys(modules).filter((id) => modules[id].valid(query)).map(async (id) => {
-        results = results.concat((await modules[id].search(query)).map((option) => ({ ...option, module: id }))).sort((a, b) => b.quality - a.quality);
+        results = results
+            .concat((await modules[id].search(query)).map((option) => ({ ...option, module: id })))
+            .sort((a, b) => b.quality - a.quality)
+            .slice(0, config.general.max_results);
         callback(results);
     }));
     callback(results);
