aa67226adf200a6feb43cbfc7ed397fa62d2a8f8
Roland Bernard <rolli.bernard@gmail.com>
2020-07-13 19:51:01 +0200

    Fixed the clipboard module

--- src/main/modules/clipboard.js
+++ src/main/modules/clipboard.js
@@ -5,25 +5,23 @@ import { config } from '../config';
 import { stringMatchQuality } from '../../common/util';
 import { getTranslation } from '../../common/local/locale';
 import path from 'path';
-import { mergeDeep } from '../../common/util';
 
 let clipboard_history = [];
 
 const clipboard_filename = 'clipboard.json';
 
-export function loadClipboard() {
+function loadClipboard() {
     const clipboard_path = path.join(app.getPath('userData'), clipboard_filename);
-    if (existsSync(path)) {
+    if (existsSync(clipboard_path)) {
         try {
-            clipboard_history = mergeDeep(clipboard_history, JSON.parse(readFileSync(clipboard_path, { encoding: 'utf8' })));
+            clipboard_history = JSON.parse(readFileSync(clipboard_path, { encoding: 'utf8' }));
         } catch (e) { }
     }
     writeFileSync(clipboard_path, JSON.stringify(clipboard_history), { encoding: 'utf8' });
 }
 
-export function updateClipboard(new_config) {
+function updateClipboard() {
     const clipboard_path = path.join(app.getPath('userData'), clipboard_filename);
-    clipboard_history = mergeDeep(clipboard_history, new_config);
     writeFileSync(clipboard_path, JSON.stringify(clipboard_history), { encoding: 'utf8' });
 }
 
@@ -37,6 +35,7 @@ const ClipboardModule = {
                 const text = clipboard.readText();
                 if (text && clipboard_history[0] !== text) {
                     clipboard_history = [text].concat(clipboard_history).slice(0, config.modules.clipboard.maximum_history);
+                    updateClipboard();
                 }
             }, config.modules.clipboard.refresh_time);
         }
@@ -47,7 +46,6 @@ const ClipboardModule = {
     },
     deinit: async () => {
         clearInterval(interval);
-        updateClipboard(clipboard_history);
     },
     valid: (query) => {
         return config.modules.clipboard.active && query.length > 0;
