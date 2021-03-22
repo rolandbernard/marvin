7104985ce2d2c188eacaab58cb172cba7deaca7d
Roland Bernard <rolli.bernard@gmail.com>
2021-03-22 20:58:38 +0100

    Brought back the scrollbar

--- src/renderer/common/output-list.js
+++ src/renderer/common/output-list.js
@@ -32,6 +32,7 @@ class OutputList extends React.Component {
             root: {
                 zIndex: 1000,
                 width: '100%',
+                height: '100%',
                 flex: '1 1 auto',
                 overflow: 'auto',
                 background: this.props.config && this.props.config.theme.background_color_output,
