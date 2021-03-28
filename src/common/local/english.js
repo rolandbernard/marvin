15d36862c3a8d6002ecd85e224af49e1df24b29f
Roland Bernard <rolli.bernard@gmail.com>
2021-03-28 14:12:34 +0200

    Fixed some spelling mistakes

--- src/common/local/english.js
+++ src/common/local/english.js
@@ -42,7 +42,7 @@ const TRANSLATION_ENGLISH = {
     quality: 'Priority',
 
     linux_system: 'Linux system',
-    linux_system_description: 'This module will provide some fundamantal linux system commands',
+    linux_system_description: 'This module will provide some fundamental linux system commands',
     reboot: 'Reboot',
     shutdown: 'Shutdown',
 
@@ -123,12 +123,12 @@ const TRANSLATION_ENGLISH = {
     color_preview: 'Color preview',
 
     web_search: 'Web search',
-    web_search_description: 'This module allows you to quicky do a web search',
+    web_search_description: 'This module allows you to quickly do a web search',
     patterns: 'Patterns',
     url_pattern: 'URL',
 
     alias: 'Alias',
-    alias_description: 'This module allows you to add an alias for a certail option, that allows you to search it under a different name',
+    alias_description: 'This module allows you to add an alias for a certain option, that allows you to search it under a different name',
     aliases: 'Aliases',
     option: 'Option',
     prefix_text: 'Add name as prefix',
