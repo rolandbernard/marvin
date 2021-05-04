7df399801ff6a7169c69b1a0422a0330dd954361
Roland Bernard <rolli.bernard@gmail.com>
2021-05-04 22:15:50 +0200

    Cleaned up the bookmarks module

--- src/main/modules/bookmarks.js
+++ src/main/modules/bookmarks.js
@@ -4,7 +4,7 @@ import { config } from "../config";
 import { exec } from 'child_process';
 import { getAllTranslation, getTranslation } from "../../common/local/locale";
 import { Database, OPEN_READONLY } from 'sqlite3';
-import { readFileSync, existsSync, readdirSync } from 'fs';
+import { readFile, access, readdir } from 'fs/promises';
 import { join } from 'path';
 import { app } from 'electron';
 import { decompressBlock } from 'lz4js';
@@ -38,11 +38,13 @@ async function getChromiumBookmarks() {
     const files = [
         join(app.getPath('home'), '.config/chromium/Default/Bookmarks'),
         join(app.getPath('home'), '.config/google-chrome/Default/Bookmarks'),
-    ].filter((file) => existsSync(file));
+    ];
     let ret = []
     for (const file of files) {
-        const bookmarks = JSON.parse(readFileSync(file, { encoding: 'utf8' }));
-        ret = ret.concat(recursiveBookmarkSearch(bookmarks));
+        try {
+            const bookmarks = JSON.parse(await readFile(file, { encoding: 'utf8' }));
+            ret = ret.concat(recursiveBookmarkSearch(bookmarks));
+        } catch (e) { /* Ignore errors */ }
     }
     return ret;
 }
@@ -50,55 +52,54 @@ async function getChromiumBookmarks() {
 async function getMidoriBookmarks() {
     const files = [
         join(app.getPath('home'), '.config/midori/bookmarks.db'),
-    ].filter((file) => existsSync(file));
-    return (await Promise.all(files.map((file) => {
-        return new Promise((res) => {
-            const db = new Database(file, OPEN_READONLY, (err) => {
-                if (err) {
-                    res([]);
-                } else {
-                    db.all(`
-                        Select title, uri url
-                            From bookmarks;
-                    `, (err, rows) => {
-                        if (err) {
-                            return res([]);
-                        } else {
-                            return res(rows);
-                        }
-                    });
-                }
-                db.close();
+    ];
+    return (await Promise.all(files.map(async file => {
+        try {
+            await access(file);
+            return await new Promise((res) => {
+                const db = new Database(file, OPEN_READONLY, (err) => {
+                    if (err) {
+                        res([]);
+                    } else {
+                        db.all(`
+                            Select title, uri url
+                                From bookmarks;
+                        `, (err, rows) => {
+                            if (err) {
+                                return res([]);
+                            } else {
+                                return res(rows);
+                            }
+                        });
+                    }
+                    db.close();
+                });
             });
-        });
+        } catch (e) {
+            return [];
+        }
     }))).flat();
 }
 
 async function getFirefoxBookmarks() {
     const firefox_dir = join(app.getPath('home'), '.mozilla/firefox');
-    if (existsSync(firefox_dir)) {
-        const files = readdirSync(firefox_dir)
+    try {
+        access(firefox_dir);
+        const folders = (await readdir(firefox_dir))
             .filter((file) => file.endsWith('.default'))
-            .map((file) => join(firefox_dir, file, 'bookmarkbackups'))
-            .filter((file) => existsSync(file))
-            .flatMap((folder) => {
-                const files = readdirSync(folder);
-                if (files.length > 0) {
-                    return join(folder, files.sort().pop())
-                } else {
-                    return null;
-                }
-            }).filter((file) => file);
-        return (await Promise.all(files.map((file) => {
-            return new Promise((res) => {
-                const data = readFileSync(file);
-                const decompressed = Buffer.alloc(data.readUInt32LE(8));
-                decompressBlock(data, decompressed, 12, data.length - 12, 0);
-                const bookmarks = JSON.parse(decompressed.toString());
-                res(recursiveBookmarkSearch(bookmarks));
-            });
+            .map((file) => join(firefox_dir, file, 'bookmarkbackups'));
+        const files = await Promise.all(folders.map(async folder => {
+            const files = await readdir(folder);
+            return join(folder, files.sort().pop())
+        }))
+        return (await Promise.all(files.map(async file => {
+            const data = await readFile(file);
+            const decompressed = Buffer.alloc(data.readUInt32LE(8));
+            decompressBlock(data, decompressed, 12, data.length - 12, 0);
+            const bookmarks = JSON.parse(decompressed.toString());
+            return recursiveBookmarkSearch(bookmarks);
         }))).flat();
-    } else {
+    } catch (e) {
         return [];
     }
 }
