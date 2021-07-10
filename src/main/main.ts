7fa5f05a6bf281ecb9603a37964200f61771a866
Roland Bernard <rolli.bernard@gmail.com>
2021-07-10 16:25:48 +0200

    Basic setup

---
+++ src/main/main.ts
@@ -0,0 +1,5 @@
+
+export {}
+
+console.log('main.ts');
+
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const devtool = process.env.NODE_ENV === 'production' ? undefined : 'inline-source-map';
+
+const commonConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    mode,
+    target: 'electron-renderer',
+    node: false,
+    devtool,
+    externals: {
+        sqlite3: 'commonjs sqlite3',
+    },
+};
+
+const mainConfig = {
+    ...commonConfig,
+    entry: path.join(__dirname, 'src', 'main', 'main.ts'),
+    output: {
+        filename: 'main.js',
+        path: path.join(__dirname, 'bundle'),
+    },
+    target: 'electron-main',
+};
+
+const rendererMainConfig = {
+    ...commonConfig,
+    entry: path.join(__dirname, 'src', 'app', 'app.ts'),
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'bundle'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'static/index.html',
+            title: 'Marvin',
+            filename: 'app.html',
+        }),
+    ],
+    target: 'electron-renderer',
+};
+
+const rendererSettingsConfig = {
+    ...commonConfig,
+    entry: path.join(__dirname, 'src', 'settings', 'settings.ts'),
+    output: {
+        filename: 'settings.js',
+        path: path.join(__dirname, 'bundle'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'static/index.html',
+            title: 'Marvin Settings',
+            filename: 'settings.html',
+        }),
+    ],
+    target: 'electron-renderer',
+}
+
+module.exports = [mainConfig, rendererMainConfig, rendererSettingsConfig];
+
