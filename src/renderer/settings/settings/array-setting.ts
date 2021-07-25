f03c2549c3a1a0ba12e4fcbb97ac5b7bb9400909
Roland Bernard <rolli.bernard@gmail.com>
2021-07-25 20:20:25 +0200

    Now using button-like for array setting

--- src/renderer/settings/settings/array-setting.ts
+++ src/renderer/settings/settings/array-setting.ts
@@ -1,5 +1,6 @@
 
 import { css, customElement, html } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
 
 import { getTranslation } from 'common/local/locale';
 import { ArrayConfig } from 'common/config-desc';
@@ -9,6 +10,7 @@ import { AbstractSetting } from 'renderer/settings/abstract-setting';
 
 import 'renderer/settings/some-setting';
 import 'renderer/common/ui/material-icon';
+import 'renderer/common/ui/button-like';
 
 @customElement('array-setting')
 export class ArraySetting extends AbstractSetting {
@@ -29,59 +31,39 @@ export class ArraySetting extends AbstractSetting {
         if (desc && this.index && this.config) {
             return this.configValue<unknown[]>()?.map((_, i) => {
                 const entry_index = this.index?.concat(i);
-                if (desc.kind === 'object') {
-                    return html`
-                        <tr class="row">
-                            <td class="setting large" colspan="2">
-                                <some-setting
-                                    .config="${this.config}"
-                                    .desc="${desc}"
-                                    .index="${entry_index}"
-                                    @update="${this.onUpdate}"
-                                ></some-setting>
-                            </td>
-                            <td class="delete">
+                const name = getTranslation(this.desc?.base?.name!, this.config);
+                const large = desc.kind === 'object';
+                const classes = classMap({
+                    'setting': true,
+                    'large': large,
+                });
+                return html`
+                    <tr class="row">
+                        ${large ? undefined : html`<td class="name">${name}</td>`}
+                        <td class="${classes}" colspan="${large ? 2 : 1}">
+                            <some-setting
+                                .config="${this.config}"
+                                .desc="${desc}"
+                                .index="${entry_index}"
+                                @update="${this.onUpdate}"
+                            ></some-setting>
+                        </td>
+                        <td class="delete">
+                            <button-like class="button-like">
                                 <material-icon
                                     class="button"
                                     name="delete"
                                     @click="${() => this.onDelete(i)}"
                                 ></material-icon>
-                            </td>
-                        </tr>
-                        <tr class="row">
-                            <td colspan="2">
-                                <div class="divider"></div>
-                            </td>
-                        </tr>
-                    `;
-                } else {
-                    const name = getTranslation(this.desc?.base?.name!, this.config);
-                    return html`
-                        <tr class="row">
-                            <td class="name">${name}</td>
-                            <td class="setting">
-                                <some-setting
-                                    .config="${this.config}"
-                                    .desc="${desc}"
-                                    .index="${entry_index}"
-                                    @update="${this.onUpdate}"
-                                ></some-setting>
-                            </td>
-                            <td class="delete">
-                                <material-icon
-                                    class="button"
-                                    name="delete"
-                                    @click="${() => this.onDelete(i)}"
-                                ></material-icon>
-                            </td>
-                        </tr>
-                        <tr class="row">
-                            <td colspan="2">
-                                <div class="divider"></div>
-                            </td>
-                        </tr>
-                    `;
-                }
+                            </button-like>
+                        </td>
+                    </tr>
+                    <tr class="row">
+                        <td colspan="2">
+                            <div class="divider"></div>
+                        </td>
+                    </tr>
+                `;
             });
         }
     }
@@ -122,20 +104,21 @@ export class ArraySetting extends AbstractSetting {
             .large {
                 padding-left: 0.8rem;
             }
-            .button {
+            .button-like {
+                display: block;
                 margin: 0.25rem;
+                border-radius: 50%;
+                overflow: hidden;
+            }
+            .button {
                 width: 3rem;
                 height: 3rem;
                 border-radius: 50%;
                 font-size: 1.5rem;
-                background: var(--settings-background);
                 color: var(--settings-accent-color);
                 cursor: pointer;
                 transition: var(--transition);
                 transition-property: background, color; 
-            }
-            .button:hover {
-                background: var(--settings-hover-background);
             }
         `;
     }
@@ -149,11 +132,13 @@ export class ArraySetting extends AbstractSetting {
                         <td></td>
                         <td></td>
                         <td class="add">
-                            <material-icon
-                                class="button"
-                                name="add"
-                                @click="${this.onNew}"
-                            ></material-icon>
+                            <button-like class="button-like">
+                                <material-icon
+                                    class="button"
+                                    name="add"
+                                    @click="${this.onNew}"
+                                ></material-icon>
+                            </button-like>
                         </td>
                     </tr>
                 </table>
